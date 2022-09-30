b2e9db3d6df8fd2d8bd7ca1f61cad818033ad064
eyusufatik <esadyusufatik@gmail.com>
2022-09-30 22:45:25 +0300

    impl matching fund

---
+++ contracts/contracts/MatchingFund.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract MatchingFund is Ownable {
+    //  No donations after payout
+    bool public paidOut;
+
+    event Payout(address payoutCtc, uint256 amount);
+    event Donation(address donator, uint256 amount);
+
+    constructor() {
+
+    }
+
+    function donate() public payable {
+        require(!paidOut, "Pool already paid out!")
+        emit Donation(msg.sender, msg.value);
+    }
+
+    function payout(address _payoutAddr) payable onlyOwner {
+        paidOut = true;
+        emit Payout(_payoutAddr, address(this).balance);
+    }
+}
