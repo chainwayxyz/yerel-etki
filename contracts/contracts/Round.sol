426d7910af0efd928d1275a9d9d096190f4622ee
eyusufatik <esadyusufatik@gmail.com>
2022-09-30 20:27:36 +0300

    impl. grant struct

--- contracts/contracts/Round.sol
+++ contracts/contracts/Round.sol
@@ -10,6 +10,23 @@ contract RoundContract is Ownable {
     IERC20 public immutable donationToken;
     address payoutContractAddr; // maybe impl. interface
 
+    struct Grant {
+        uint16 id;
+        address owner;
+        uint48 createdAt; // timestamp the grant was created, uint48 max-> year 8.7 million or so
+        uint48 lastUpdated; // timestamp the grant data was last updated
+        address payee; // address that receives funds donated to this grant
+        string ipfsURL;
+    }
+
+    //  to keep ids 
+    uint16 public grantCount;
+
+    //  id -> grant
+    mapping(uint16 => Grant) public grants;
+    //  id -> donations
+    mapping(uint16 => uint256[]) public grantDonations;
+
     constructor(uint256 _startTime, uint256 _endTime, IERC20 _donationToken, address _payoutContractAddr) {
         startTime = _startTime;
         endTime = _endTime;
