a59eb8622a9f56c49e251754048846fa6ceef4ed
orkunkilic <orkunmahirkilic@gmail.com>
2022-10-01 16:56:18 +0300

    add subnet

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -10,6 +10,13 @@ require("@nomicfoundation/hardhat-toolbox");
 module.exports = {
   solidity: "0.8.7",
   networks: {
+    subnet: {
+      chainId: 90,
+      url: "http://127.0.0.1:9650/ext/bc/nrdK64Xks6hrDHpahY4sHEy6TaBRuhpYecURTbeuvoBmkybkd/rpc",
+      accounts: [
+        process.env.PRIVATE_KEY
+      ]
+    },
     hardhat: {
       chainId: 1337 // We set 1337 to make interacting with MetaMask simpler
     }
